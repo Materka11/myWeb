a8c9505a5bc2d0353aabcac25b537b4925fb37ac
Materka11 <arekmaterka11@gmail.com>
2022-06-30 19:34:33 +0200

    Create head in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,6 +11,28 @@ function Header() {
 				<span>MENU</span>
 				<a href="mailto:arekmaterka11@gmail.com?subject=Hi Arek, I'd like to say hello">SAY HELLO</a>
 			</nav>
+			<div className="head">
+				<div className="hair" />
+				<div className="face">
+					<div className="eyebrows left" />
+					<div className="eyebrows right" />
+					<div className="eyes left" />
+					<div className="eyes right" />
+					<div className="lowerEyelid left" />
+					<div className="lowerEyelid right" />
+					<div className="nose" />
+					<div className="mouth" />
+				</div>
+				<div className="ears left">
+					<div className="line" />
+				</div>
+				<div className="ears right">
+					<div className="line" />
+				</div>
+				<div className="neck left" />
+				<div className="neck right" />
+				<div className="body" />
+			</div>
 		</div>
 	);
 }
